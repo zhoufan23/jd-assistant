810ca30308a406f1aa9e6808e4e741238df6495c
zhoufan <zhoufan@zhihu.com>
2020-06-22 06:49:19 +0800

    for maotai

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     https://github.com/tychxn/jd-assistant/wiki/1.-%E4%BA%AC%E4%B8%9C%E6%8A%A2%E8%B4%AD%E5%8A%A9%E6%89%8B%E7%94%A8%E6%B3%95
     """
 
-    sku_ids = '100001324422'  # 商品id
-    area = '1_72_4211'  # 区域id
-    asst = Assistant()  # 初始化
-    asst.login_by_QRcode()  # 扫码登陆
-    asst.buy_item_in_stock(sku_ids=sku_ids, area=area, wait_all=False, stock_interval=5)  # 根据商品是否有货自动下单
+    # sku_ids = '54695063302:2'  # 商品id
+    # area = '1_72_55657'  # 区域id
+    # asst = Assistant()  # 初始化
+    # asst.login_by_QRcode()  # 扫码登陆
+    # asst.buy_item_in_stock(sku_ids=sku_ids, area=area, wait_all=False, stock_interval=5)  # 根据商品是否有货自动下单
     # 6个参数：
     # sku_ids: 商品id。可以设置多个商品，也可以带数量，如：'1234' 或 '1234,5678' 或 '1234:2' 或 '1234:2,5678:3'
     # area: 地区id
@@ -20,3 +20,13 @@ if __name__ == '__main__':
     # stock_interval: 查询库存时间间隔，可选参数，默认3秒
     # submit_retry: 提交订单失败后重试次数，可选参数，默认3次
     # submit_interval: 提交订单失败后重试时间间隔，可选参数，默认5秒
+
+
+    asst = Assistant()      # 初始化
+    asst.login_by_QRcode()  # 扫码登陆
+    asst.add_item_to_cart(sku_ids='100012043978')  # 根据商品id添加购物车（可选）
+    asst.submit_order_by_time(buy_time='2020-06-22 9:59:59.900', retry=3, interval=0.1)  # 定时提交订单
+    # 3个参数：
+    # buy_time: 下单时间，例如：'2019-02-16 01:17:59.500'
+    # retry: 下单重复执行次数，可选参数，默认4次
+    # interval: 下单执行间隔，可选参数，默认5秒
--- timer.py
+++ timer.py
@@ -7,7 +7,7 @@ from log import logger
 
 class Timer(object):
 
-    def __init__(self, buy_time, sleep_interval=0.5):
+    def __init__(self, buy_time, sleep_interval=0.05):
 
         # '2018-09-28 22:45:50.000'
         self.buy_time = datetime.strptime(buy_time, "%Y-%m-%d %H:%M:%S.%f")
